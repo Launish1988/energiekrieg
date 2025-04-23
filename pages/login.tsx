0743afdb6d2800a4c51294c939a9cfa71573e04f
Launish1988 <kaiuwehaase@hotmail.de>
2025-04-23 23:07:18 +0200

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -11,90 +11,83 @@ export default function LoginPage() {
   async function handleSubmit(e: FormEvent) {
     e.preventDefault();
     setError("");
-    try {
-      const res = await fetch("/api/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
-      if (res.ok) {
-        router.push("/");
-      } else {
-        const data = await res.json();
-        setError(data.msg || "Login fehlgeschlagen");
-      }
-    } catch (err) {
-      setError("Netzwerkfehler");
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    if (res.ok) {
+      router.push("/");
+    } else {
+      const data = await res.json();
+      setError(data.msg || "Login fehlgeschlagen");
     }
   }
 
   return (
     <div
-      className="relative w-screen h-screen"
-      style={{
-        backgroundImage: 'url("/images/login-bg.jpg")',
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-      }}
+      className="relative w-full h-screen bg-cover bg-center"
+      style={{ backgroundImage: 'url("/images/login-bg.jpg")' }}
     >
-      <form onSubmit={handleSubmit} className="relative w-full h-full">
-        {/* E-Mail Eingabe über dem goldenen Feld */}
+      <form onSubmit={handleSubmit} className="absolute inset-0">
+        {/* E-Mail (Position nach Bild anpassen!) */}
         <input
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           className="
-            absolute top-[36%] left-1/2 -translate-x-1/2
+            absolute
+            top-[35%] left-1/2 transform -translate-x-1/2
             w-[70%] max-w-xs h-10
             bg-transparent text-white placeholder-transparent
             border-none focus:outline-none
           "
+          placeholder=""
         />
 
-        {/* Passwort Eingabe */}
+        {/* Passwort */}
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           className="
-            absolute top-[46%] left-1/2 -translate-x-1/2
+            absolute
+            top-[45%] left-1/2 transform -translate-x-1/2
             w-[70%] max-w-xs h-10
             bg-transparent text-white placeholder-transparent
             border-none focus:outline-none
           "
+          placeholder=""
         />
 
-        {/* Login Button (deckungsgleich mit dem goldenen Button) */}
+        {/* Log In Button */}
         <button
           type="submit"
           className="
-            absolute top-[56%] left-1/2 -translate-x-1/2
+            absolute
+            top-[55%] left-1/2 transform -translate-x-1/2
             w-[60%] max-w-xs h-12
             bg-transparent border-none
             cursor-pointer
           "
-        >
-          {/* Leer lassen – der sichtbare Button liegt im Hintergrundbild */}
-        </button>
+        />
 
         {/* Create Account Button */}
         <button
           type="button"
           onClick={() => router.push("/register")}
           className="
-            absolute top-[68%] left-1/2 -translate-x-1/2
+            absolute
+            top-[67%] left-1/2 transform -translate-x-1/2
             w-[60%] max-w-xs h-12
             bg-transparent border-none
             cursor-pointer
           "
-        >
-          {/* Leer lassen */}
-        </button>
+        />
 
         {/* Fehlermeldung */}
         {error && (
-          <p className="absolute top-[80%] left-1/2 -translate-x-1/2 text-red-400">
+          <p className="absolute top-[80%] left-1/2 transform -translate-x-1/2 text-red-400">
             {error}
           </p>
         )}
