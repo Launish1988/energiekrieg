b14440a7f4c7fe9448c55560aab447ca308c51d7
Launish1988 <kaiuwehaase@hotmail.de>
2025-04-23 20:20:46 +0200

    Create mongo.ts

---
+++ lib/mongo.ts
@@ -0,0 +1,17 @@
+// lib/mongo.ts
+import { MongoClient, Db } from "mongodb";
+
+const uri = process.env.MONGODB_URI!;
+const options: any = {};
+
+let client: MongoClient;
+let db: Db;
+
+/** Liefert eine singleton-DB-Instanz (Serverless-freundlich) */
+export async function getDb() {
+  if (db) return db;
+  client = client ?? new MongoClient(uri, options);
+  await client.connect();
+  db = client.db(process.env.MONGODB_DB || "energiekrieg");
+  return db;
+}
